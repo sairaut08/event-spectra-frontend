18b2c81458b41dab3da8db148f9db9616ebc95a8
sairaut08 <rautsai08@gmail.com>
2024-05-05 14:29:36 +0530

    new link added

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const axiosInstance = axios.create()
 
-axiosInstance.defaults.baseURL = 'http://localhost:7006/api/v1'
+axiosInstance.defaults.baseURL = 'https://event-spectra-backend-nodejs.onrender.com/api/v1'
 axiosInstance.defaults.withCredentials = true
 
 export default axiosInstance
